ebbd313d823770d50893a7945e2eb545a2b6a531
Luis Garcia <lalejandro.1988@gmail.com>
2021-04-09 16:18:40 -0600

    Contador Screen

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
+import { CounterScreen } from './src/screens/CounterScreen';
+
+/*
+** Punto inicial de la aplicacion
+*/
+export const App = () => {
+  return (
+    // <HolaMundoScreen />
+    <CounterScreen />
+  )
+}
+
